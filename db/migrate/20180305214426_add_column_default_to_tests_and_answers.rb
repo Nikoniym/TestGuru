906f6a0ef5f6f809e4dabb2a4bd4fd2c6860727b
nik <worldnillin@gmail.com>
2018-03-07 04:16:26 +0300

    correction 1

--- db/migrate/20180305214426_add_column_default_to_tests_and_answers.rb
+++ db/migrate/20180305214426_add_column_default_to_tests_and_answers.rb
@@ -1,11 +1,7 @@
 class AddColumnDefaultToTestsAndAnswers < ActiveRecord::Migration[5.1]
-  def up
+  def change
     add_column :answers, :correct, :boolean, default:  false
-    change_column :tests, :level, :integer, default: 1
-  end
-
-  def down
-    remove_column :answers, :correct
-    change_column :tests, :level, :integer, default: nil
+    change_column_null(:answers, :correct, false)
+    change_column_default :tests, :level, from: nil, to: 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305214426) do
+ActiveRecord::Schema.define(version: 20180307004403) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
+    t.integer "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -28,17 +29,19 @@ ActiveRecord::Schema.define(version: 20180305214426) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "test_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 1
-    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
