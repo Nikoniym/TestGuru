ef492559afbd6d8838542cc81c1b219ec3871064
nik <worldnillin@gmail.com>
2018-03-16 21:03:18 +0300

    wor ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :tests
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+
   def tests_with_level(difficulty)
     Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
         .where(level: difficulty, user_tests: { user_id: self.id })
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312170831) do
+ActiveRecord::Schema.define(version: 20180316172257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 20180312170831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "user_tests", force: :cascade do |t|
@@ -62,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180312170831) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tests", "users"
   add_foreign_key "user_tests", "tests"
   add_foreign_key "user_tests", "users"
 end
