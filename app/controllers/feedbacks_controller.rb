1cc82b490b3b54128a0414d6c405a2856ae82ab8
nik <worldnillin@gmail.com>
2018-05-05 03:03:37 +0300

    correction 3

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,2 +1,21 @@
 class FeedbacksController < ApplicationController
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    @feedback.user = current_user
+    if @feedback.save
+      redirect_to  new_feedback_path, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:title, :body)
+  end
 end
