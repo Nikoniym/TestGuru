02ca10e6d349d314053c155abd4ed3022bf2ee85
nik <worldnillin@gmail.com>
2018-05-08 15:50:49 +0300

    timer ready

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -61,6 +61,6 @@ class  Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :timer)
   end
 end
